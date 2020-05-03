07eb689681580e2f4c5b2f6988c2305cc6b44080
Zakhar <kot.zakhar@gmail.com>
2020-05-04 00:42:41 +0300

    Implementing "Add wifi credentials"

--- app/src/main/java/com/zakhar/watersensorapp/wifiActivity/WifiActivity.kt
+++ app/src/main/java/com/zakhar/watersensorapp/wifiActivity/WifiActivity.kt
@@ -1,16 +1,16 @@
 package com.zakhar.watersensorapp.wifiActivity
 
 import android.bluetooth.BluetoothSocket
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.transition.Visibility
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.zakhar.watersensorapp.CurrentBluetoothDevice
 import com.zakhar.watersensorapp.R
 import kotlinx.android.synthetic.main.activity_wifi.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlinx.serialization.builtins.list
 import kotlinx.serialization.json.Json
 import kotlin.coroutines.CoroutineContext
@@ -51,5 +51,39 @@ class WifiActivity: AppCompatActivity(), CoroutineScope {
     override fun onDestroy() {
         job.cancel()
         super.onDestroy()
+    }
+
+    fun toggleNewWifiForm(view: View) {
+        if (activity_wifi__new_record__layout.visibility == View.VISIBLE) {
+            activity_wifi__new_record__layout.visibility = View.GONE
+        } else {
+            activity_wifi__new_record__layout.visibility = View.VISIBLE
+        }
+    }
+
+    fun createNewWifiRecord(view: View) {
+        val ssid = activity_wifi__new_record__ssid__edit_text.text.toString().trim()
+        val password = activity_wifi__new_record__password__edit_text.text.toString().trim()
+        if (ssid.isEmpty())
+            return
+
+        val newWifiRecord = WifiRecord(ssid, password)
+        val serializedRecord = Json.stringify(WifiRecord.serializer(), newWifiRecord);
+        Log.d(TAG, serializedRecord)
+
+        launch {
+            CurrentBluetoothDevice.sendCommand("json:new wifi")
+            delay(1000)
+            val result = CurrentBluetoothDevice.sendCommandWithFeedback(serializedRecord)
+            if (result.trim().toLowerCase() == "ok") {
+                activity_wifi__new_record__ssid__edit_text.text.clear()
+                activity_wifi__new_record__password__edit_text.text.clear()
+                toggleNewWifiForm(activity_wifi__new_record__confirm__image_button)
+                val adapter = activity_wifi__wifi_records__list_view.adapter as WifiArrayAdapter
+                adapter.add(newWifiRecord)
+            } else {
+                Log.e(TAG, result)
+            }
+        }
     }
 }
